b4a5a0e7883b2b023a6327de5227a69894544848
Bryant Curto <bryantcurto@gmail.com>
2021-02-05 00:24:42 -0500

    Updated testing script to handle different yielding tests and more

--- tests/quick_test.sh
+++ tests/quick_test.sh
@@ -14,6 +14,7 @@ EVENTS="L1-dcache-loads,L1-dcache-load-misses"
 
 trap exit SIGINT
 
+TESTS= # stores the tests to run
 run_tests() {
 	# Before test(s) begin:
 	sudo cpupower frequency-set -g performance > /dev/null
@@ -27,14 +28,17 @@ run_tests() {
 	    fi
 
 	    test_prefix=$(echo "$test" | cut -d ' ' -f 1)
-	    command="$(echo "$test" | cut -d ' ' -f 2-)"
-	    command="$command -r $SEED -d $duration $WORKING_SET_SIZE_KB"
+	    options="$(echo "$test" | cut -d ' ' -f 2-)"
+	    cmd="$CACHETEST $options -r $SEED -d $duration $WORKING_SET_SIZE_KB"
+	    cmd="taskset -ac 0-15 sudo perf stat -e $EVENTS $cmd"
+
 	    output_filepath="$OUTPUT_DIR/$test_prefix"_duration_"$duration".out
 
 	    echo "Running $test_prefix ($duration sec) - $(date -u)"
-	    git log -1 --pretty=format:"Commit Hash: %h - %s" > "$output_filepath"
-	    echo >> "$output_filepath"
-	    eval "taskset -ac 0-15 sudo perf stat -e $EVENTS $command" &>> "$output_filepath"
+	    echo $(git log -1 --pretty=format:"Commit Hash: %h - %s") > "$output_filepath"
+		echo "$cmd" >> "$output_filepath"
+		echo >> "$output_filepath"
+	    eval "$cmd" &>> "$output_filepath"
 	  done <<< "$(echo -e "$TESTS")"
 	done
 
@@ -78,9 +82,36 @@ build_cachetest() {
 	popd
 }
 
+set_option() {
+	option="$1"
+	action="$2"
+
+	if [[ 0 == "$action" ]]; then
+		start_pre=
+		end_pre='//'
+		str="disable"
+	else
+		start_pre='//'
+		end_pre=
+		str="enable"
+	fi
+	start_sed_pre=$(echo $start_pre | sed 's/\//\\\//g')
+	end_sed_pre=$(echo $end_pre | sed 's/\//\\\//g')
+
+	if [[ 1 != $(cat "$CACHETEST_SRC" | grep '^'"$start_pre"'#define '"$option"'$' | wc -l) ]]; then
+		echo "Failed to $str $option"
+		exit -1
+	fi
+
+	start_str="$start_sed_pre"'\(#define '"$option"'\)'
+	end_str="$end_sed_pre"'\1'
+	cat "$CACHETEST_SRC" | sed 's/^'"$start_str"'$/'"$end_str"'/' > "$CACHETEST"
+	mv "$CACHETEST" "$CACHETEST_SRC"
+
+	build_cachetest
+}
+
 # Run the test with working set size that is ~1.5 * size of L1.
-
-
 # TODO: Vary the sizes, checking hits/misses for L2 and L3
 L1_SIZE_SCALAR=1.5
 L1_SIZE_B=$(get_cache_size 1 Data)
@@ -95,38 +126,39 @@ build_cachetest
 TESTS=
 
 ## Baseline test
-TESTS="$TESTS""baseline $CACHETEST -T 16\n"
-TESTS="$TESTS""baseline_group_pinned $CACHETEST -T 16 -C 0-15\n"
-TESTS="$TESTS""baseline_indiv_pinned $CACHETEST -T 16 -C 0-15 -I\n"
+TESTS="$TESTS""baseline -T 16\n"
+TESTS="$TESTS""baseline_group_pinned -T 16 -C 0-15\n"
+TESTS="$TESTS""baseline_indiv_pinned -T 16 -C 0-15 -I\n"
 
 ## Thread test
-TESTS="$TESTS""threads $CACHETEST -T 8,8\n"
-TESTS="$TESTS""threads_group_pinned $CACHETEST -T 8,8 -C 0-7.8-15\n"
-TESTS="$TESTS""threads_indiv_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -I\n"
+TESTS="$TESTS""threads -T 8,8\n"
+TESTS="$TESTS""threads_group_pinned -T 8,8 -C 0-7.8-15\n"
+TESTS="$TESTS""threads_indiv_pinned -T 8,8 -C 0-7.8-15 -I\n"
 
 ## Fibre test
-TESTS="$TESTS""fibres $CACHETEST -T 8,8 -F 32,32\n"
-TESTS="$TESTS""fibres_group_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -F 32,32\n"
-TESTS="$TESTS""fibres_indiv_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -I -F 32,32\n"
+TESTS="$TESTS""fibres32 -T 8,8 -F 32,32\n"
+TESTS="$TESTS""fibres32_group_pinned -T 8,8 -C 0-7.8-15 -F 32,32\n"
+TESTS="$TESTS""fibres32_indiv_pinned -T 8,8 -C 0-7.8-15 -I -F 32,32\n"
 
 ## Migration test
-TESTS="$TESTS""migrate $CACHETEST -T 8,8 -F 32,32 -M\n"
-TESTS="$TESTS""migrate_group_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -F 32,32 -M\n"
-TESTS="$TESTS""migrate_indiv_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -I -F 32,32 -M\n"
+TESTS="$TESTS""migrate32 -T 8,8 -F 32,32 -M\n"
+TESTS="$TESTS""migrate32_group_pinned -T 8,8 -C 0-7.8-15 -F 32,32 -M\n"
+TESTS="$TESTS""migrate32_indiv_pinned -T 8,8 -C 0-7.8-15 -I -F 32,32 -M\n"
+
+
+# Vary the number of fibres
+for f in 8 16 64 128; do
+	TESTS="$TESTS""fibres$f -T 8,8 -F $f,$f\n"
+	TESTS="$TESTS""migrate$f -T 8,8 -F $f,$f -M\n"
+done
 
 ### RUN THE TESTS
 run_tests
 
 
-### Enable yielding
-if [[ 1 != $(cat "$CACHETEST_SRC" | grep '^//#define FIBRE_YIELD$' | wc -l) ]]; then
-	echo "Failed to enable yielding"
-	exit -1
-fi
 
-cat "$CACHETEST_SRC" | sed 's/^\/\/\(#define FIBRE_YIELD\)$/\1/' > "$CACHETEST"
-mv "$CACHETEST" "$CACHETEST_SRC"
-build_cachetest
+### Enable yielding
+set_option FIBRE_YIELD 1
 
 ### Yielding-Fibre-Code Tests
 ## Create 16 fibres per subpath, no migration just loop over subpath, fibres yield at the end of subpath
@@ -134,22 +166,47 @@ build_cachetest
 TESTS=
 
 ## Evaluate the cost of calling yield, but not actually switching to another fibre
-TESTS="$TESTS""yield_fibres_baseline $CACHETEST -T 8,8 -F 8,8\n"
-TESTS="$TESTS""yield_fibres_baseline_group_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -F 8,8\n"
-TESTS="$TESTS""yield_fibres_baseline_indiv_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -I -F 8,8\n"
+TESTS="$TESTS""yield_fibres8_baseline -T 8,8 -F 8,8\n"
+TESTS="$TESTS""yield_fibres8_baseline_group_pinned -T 8,8 -C 0-7.8-15 -F 8,8\n"
+TESTS="$TESTS""yield_fibres8_baseline_indiv_pinned -T 8,8 -C 0-7.8-15 -I -F 8,8\n"
 
 ## Evaluate the cost of calling yield and switching to another fibre
-TESTS="$TESTS""yield_fibres $CACHETEST -T 8,8 -F 32,32\n"
-TESTS="$TESTS""yield_fibres_group_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -F 32,32\n"
-TESTS="$TESTS""yield_fibres_indiv_pinned $CACHETEST -T 8,8 -C 0-7.8-15 -I -F 32,32\n"
+TESTS="$TESTS""yield_fibres32 -T 8,8 -F 32,32\n"
+TESTS="$TESTS""yield_fibres32_group_pinned -T 8,8 -C 0-7.8-15 -F 32,32\n"
+TESTS="$TESTS""yield_fibres32_indiv_pinned -T 8,8 -C 0-7.8-15 -I -F 32,32\n"
 
 ### RUN THE TESTS
 run_tests
 
 ### Disable yielding
-cat "$CACHETEST_SRC" | sed 's/^\(#define FIBRE_YIELD\)$/\/\/\1/' > "$CACHETEST"
-mv "$CACHETEST" "$CACHETEST_SRC"
-build_cachetest
+set_option FIBRE_YIELD 0
+
+
+
+## Enable global yielding
+set_option FIBRE_YIELD_GLOBAL 1
+
+## Evaluate the cost of calling yield and switching to another fibre
+TESTS="yield_global_fibres32 -T 8,8 -F 32,32\n"
+
+### RUN THE TESTS
+run_tests
+
+### Disable global yielding
+set_option FIBRE_YIELD_GLOBAL 0
+
+
+### Enable global yielding
+set_option FIBRE_YIELD_FORCE 1
+
+## Evaluate the cost of calling yield and switching to another fibre
+TESTS="yield_force_fibres32 -T 8,8 -F 32,32\n"
+
+### RUN THE TESTS
+run_tests
+
+### Disable global yielding
+set_option FIBRE_YIELD_FORCE 0
 
 
 #sudo perf record -e cycles,L1-dcache-load-misses,LLC-load-misses,cache-misses -g ./src/cachetest -T 8,8 -F 32,32 -r 2 -d 1 49 > tests/output/{yield_,}fibres_duration_1_report.txt
